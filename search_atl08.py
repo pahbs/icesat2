5f8d33aadae5a4a265686ad14410d6c9ad61e441
Paul Montesano <paul.montesano@gmail.com>
2020-02-25 15:33:28 -0500

    add argparse

--- search_atl08.py
+++ search_atl08.py
@@ -6,14 +6,26 @@
     THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.'''
 
 from maap.maap import MAAP
+import argparse
+
+def getparser():
+    parser = argparse.ArgumentParser(description='Correct CHM pixel values according to correction based on guassian peak analysis of minimum (ground) peak in CHM image histogram')
+    parser.add_argument('data_dir', type=str, help='Data directory to which you download')
+    parser.add_argument('bbox', type=str, default=None, help="Lon,lat box ('lx,ly,ux,uy')"")
+    parser.add_argument('gran_limit', type=int, default=None, help="Limit of granules to return")                    
+    return parser
 
 def searth_atl08():
+                        
+    parser = getparser()
+    args = parser.parse_args()
+    
     maap = MAAP()
     
-    s=maap.searchGranule(collection_concept_id="C1200116818-NASA_MAAP", bounding_box="95,70,100,75", limit=10)
+    s=maap.searchGranule(collection_concept_id="C1200116818-NASA_MAAP", bounding_box=args.bbox, limit=10)
     
     for f in range(len(s)):
-       s[f].getLocalPath('/projects/data/atl08')
+       s[f].getLocalPath(args.data_dir)
     
 def main():
     print("\nICESat2GRD is written by Nathan Thomas (@Nmt28).\nModified by Paul Montesano | paul.m.montesano@nasa.gov\nUse '-h' for help and required input parameters\n")
